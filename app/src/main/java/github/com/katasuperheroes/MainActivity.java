4dd3b5529571e22508872d950f1417f1367a6021
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-01-08 16:10:44 +0100

    Apply project checkstyle

--- app/src/main/java/github/com/katasuperheroes/MainActivity.java
+++ app/src/main/java/github/com/katasuperheroes/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
