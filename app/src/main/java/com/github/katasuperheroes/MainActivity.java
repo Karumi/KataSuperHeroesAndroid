fbe0b36a3e6b996b8eed654704e582227dffc710
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-01-08 16:25:37 +0100

    Fix build problem

--- app/src/main/java/com/github/katasuperheroes/MainActivity.java
+++ app/src/main/java/com/github/katasuperheroes/MainActivity.java
@@ -17,11 +17,8 @@
 package com.github.katasuperheroes;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import github.com.katasuperheroes.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,14 +28,5 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.main_activity);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
-      }
-    });
   }
 }
