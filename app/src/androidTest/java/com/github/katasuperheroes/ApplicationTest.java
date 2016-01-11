2653be7b14c6412d04154d7e80f7b8b2bc205b70
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-01-11 10:44:43 +0100

    Add espresso and other android testing dependencies to get the first Android UI test working

--- app/src/androidTest/java/com/github/katasuperheroes/ApplicationTest.java
+++ app/src/androidTest/java/com/github/katasuperheroes/ApplicationTest.java
@@ -16,14 +16,27 @@
 
 package com.github.katasuperheroes;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import com.github.katasuperheroes.ui.view.MainActivity;
+import github.com.katasuperheroes.R;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-  public ApplicationTest() {
-    super(Application.class);
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class) @LargeTest public class ApplicationTest {
+
+  @Rule public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+  @Test public void test1() {
+    activityRule.getActivity();
+
+    onView(withId(R.id.recycler_view)).check(matches(isDisplayed()));
   }
 }
