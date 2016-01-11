464905986a42c6fa3eb595e9133ba428c9a5d04a
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-01-11 15:19:05 +0100

    Test shows super heroes name inside the list view

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -27,6 +27,8 @@ import com.karumi.katasuperheroes.model.SuperHeroesRepository;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +41,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class) @LargeTest public class MainActivityTest {
+
+  private static final int ANY_NUMBER_OF_SUPER_HEROES = 3;
 
   @Rule public DaggerMockRule<MainComponent> daggerRule =
       new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
@@ -58,10 +62,42 @@ import static org.mockito.Mockito.when;
   @Mock SuperHeroesRepository repository;
 
   @Test public void showsEmptyCaseIfThereAreNoSuperHeroes() {
-    when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
+    givenThereAreNoSuperHeroes();
 
-    activityRule.launchActivity(null);
+    startActivity();
 
     onView(withText("¯\\_(ツ)_/¯")).check(matches(isDisplayed()));
+  }
+
+  @Test public void showsSuperHeroesNameIfThereAreSuperHeroes() {
+    List<SuperHero> superHeroes = givenThereAreSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
+
+    startActivity();
+
+    for (SuperHero superHero : superHeroes) {
+      onView(withText(superHero.getName())).check(matches(isDisplayed()));
+    }
+  }
+
+  private List<SuperHero> givenThereAreSuperHeroes(int numberOfSuperHeroes) {
+    List<SuperHero> superHeroes = new LinkedList<>();
+    for (int i = 0; i < numberOfSuperHeroes; i++) {
+      String superHeroName = "SuperHero - " + i;
+      String superHeroPhoto = "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg";
+      boolean isAvenger = i % 2 == 0;
+      String superHeroDescription = "Description Super Hero - " + i;
+      superHeroes.add(
+          new SuperHero(superHeroName, superHeroPhoto, isAvenger, superHeroDescription));
+    }
+    when(repository.getAll()).thenReturn(superHeroes);
+    return superHeroes;
+  }
+
+  private void givenThereAreNoSuperHeroes() {
+    when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
+  }
+
+  private void startActivity() {
+    activityRule.launchActivity(null);
   }
 }
