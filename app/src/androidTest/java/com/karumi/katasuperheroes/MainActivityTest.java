a568934643928346176b574d524caf87a7190b46
pedrovgs <pedrovicente.gomez@gmail.com>
2016-01-12 00:33:29 +0100

    Remove AnimationAwareTestRule from the this project

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -75,8 +75,6 @@ import static org.mockito.Mockito.when;
   @Rule public IntentsTestRule<MainActivity> activityRule =
       new IntentsTestRule<>(MainActivity.class, true, false);
 
-  @Rule public AnimationAwareTestRule mAnimationAwareTestRule = new AnimationAwareTestRule();
-
   @Mock SuperHeroesRepository repository;
 
   @Test public void showsEmptyCaseIfThereAreNoSuperHeroes() {
